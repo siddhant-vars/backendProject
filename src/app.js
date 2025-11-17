faa673a6c48d4ec2d197b01ef56b042652563a14
siddhant-vars <siddhantvarshney446@gmail.com>
2025-11-17 19:59:17 +0530

    some comments are added

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+app.use(cors({ //use for middleware and for config
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
@@ -12,6 +12,6 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser()) //
 
 export { app }
